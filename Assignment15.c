3e3a05024a083982efa02f91ce39a31a41713385
kumaraman-dev <amankumarnaw23@gmail.com>
2023-08-24 22:07:55 +0530

    Update Assignment15.c
    q1 and q2 done

--- Assignment15.c
+++ Assignment15.c
@@ -1,6 +1,47 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int checkPrime(int num)
+{
+    // 0, 1 and negative numbers are not prime
+    if(num < 2){
+        return 0;
+    }
+    else{   
+    // no need to run loop till num-1 as for any number x the numbers in
+    // the range(num/2 + 1, num) won't be divisible anyways. 
+    // Example 36 wont be divisible by anything b/w 19-35
+        int x = num/2;
+        for(int i = 2; i <=x; i++)
+        {
+            if(num % i == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    // the number would be prime if we reach here
+    return 1;
+}
+
+int q1()
+{
+    int a = 1, b = 100;
+    
+    for(int i=a; i <= b; i++){
+        if(checkPrime(i))
+            printf("%d ",i);
+    }
+ 
+    return 0;
+}
 int main()
 {
-    printf("Hello World");
+  int a,b;
+  printf("Enter Two Number");
+  scanf("%d%d",&a,&b);
+for(int i=a; i <= b; i++){
+        if(checkPrime(i))
+            printf("%d ",i);
+    }
     return 0;
 }
