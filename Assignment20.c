68aca0946f5e05c3f4cfd2f882774a4d817cbde7
kumaraman-dev <amankumarnaw23@gmail.com>
2023-08-25 11:02:19 +0530

    Update Assignment20.c
    q3 done

--- Assignment20.c
+++ Assignment20.c
@@ -94,3 +94,21 @@ break;
         break;
     }
 }
+int q3()
+{
+    char a;
+    printf("Enter Your Charecter");
+    scanf("%c",&a);
+    switch (a)
+    {
+    case 'A'...'Z':
+    printf("Uppercase");
+        break;
+    case 'a'...'z':
+    printf("Lowercase");
+    break;
+    default:
+    printf("Special Character");
+        break;
+    }
+}
