d5bde6cf7e23a1b7610bff85d62d896ad5261538
kumaraman-dev <amankumarnaw23@gmail.com>
2023-08-25 10:28:50 +0530

    Update Assignment20.c
    q2 done

--- Assignment20.c
+++ Assignment20.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+int q1()
 {
     int a;
     printf("\n1.If Your Mark Is Between 90 to 100 ");
@@ -32,6 +32,65 @@ int main()
     break;
     default:
     printf("Invalid Marks");
+        break;
+    }
+}
+int q2()
+{
+    int a;
+    int n,p,i,b,e,s;
+    float c,d;
+    printf("\n1.Factorial Of Number");
+    printf("\n2.Check Even And Odd");
+    printf("\n3.Area of circle");
+    printf("\n4.Sum of first n natural number");
+    printf("\n5.Exit");
+    printf("\nEnter Your Choice");
+    scanf("%d",&a);
+    switch (a)
+    {
+    case 1:
+    
+    printf("Enter The number");
+    scanf("%d",&n);
+    for ( i = 1,p=1; i <=n; i++)
+    {
+        p=p*i;
+
+    }
+    printf("%d",p);
+        break;
+    case 2:
+    printf("Enter Your Number :");
+    scanf("%d",&b);
+    if (b%2==0)
+    {
+        printf("Even Number");
+    }
+    else{
+        printf("Odd Number");
+    }
+    case 3:
+    printf("Enter Your radius of circle");
+    scanf("%f",&c);
+    d=3.14*c*c;
+    printf("%f",d);
+    break;
+    case 4:
+printf("Enter The number You Want To find Sum");
+scanf("%d",&e);
+for ( i = 1,s=0; i <=e; i++)
+{
+   s=s+i;
+   
+}
+printf("%d",s);
+break;
+case 5:
+printf("Exit");
+break;
+    default:
+    printf("default");
         break;
     }
 }
