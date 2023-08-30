0fa5269f5b2f4cf7f6c09832e59d9d6f1851d1da
kumaraman-dev <amankumarnaw23@gmail.com>
2023-08-30 09:38:25 +0530

    Update assignment22.c
    q2

--- assignment22.c
+++ assignment22.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 int factorial(int);
 int main()
 {
@@ -17,4 +17,43 @@ int factorial(int n)
       
     }
      return p;
+}*/
+#include<stdio.h>
+float combination(int,int);
+int main()
+{
+    int n,r,s;
+printf("Enter N");
+scanf("%d",&n);
+printf("Enter r");
+scanf("%d",&r);
+s= combination(n,r);
+printf("No Of Combination Is %d",s);
+}
+float combination(int n, int r)
+{
+    int z,p,q,t,i,j,k;
+if (r>n)
+{
+    printf("Wrong input");
+}
+else
+for ( i = 1,p=1; i <=n; i++)
+{
+   p=p*i;
+
+}
+for (j = 1,q=1; j <=r; j++)
+{
+    q=q*j;
+}
+for ( k = 1,t=1; k <=(n-r); k++)
+{
+   t=t*k;
+}
+z=p/(q*t);
+return z;
+
+
+
 }
