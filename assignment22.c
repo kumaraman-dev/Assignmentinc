2fc65d89d5ce5aebc53027c0e3ef39ea0fd1ac3a
kumaraman-dev <amankumarnaw23@gmail.com>
2023-08-30 09:53:51 +0530

    Update assignment22.c
    q3

--- assignment22.c
+++ assignment22.c
@@ -18,7 +18,7 @@ int factorial(int n)
     }
      return p;
 }*/
-#include<stdio.h>
+/*#include<stdio.h>
 float combination(int,int);
 int main()
 {
@@ -53,7 +53,40 @@ for ( k = 1,t=1; k <=(n-r); k++)
 }
 z=p/(q*t);
 return z;
+}*/
+#include<stdio.h>
+int arrangment(int, int);
+int main()
+{
+    int n,r,s;
+    printf("Enter n");
+    scanf("%d",&n);
+    printf("Enter r");
+    scanf("%d",&r);
+    s= arrangment(n,r);
+    printf("%d",s);
+}
+int arrangment(int n, int r)
+{
+int p,q,i,j,t;
 
-
+if (r>n)
+{
+    printf("Error");
+}
+else
+{
+    for ( i = 1,p=1; i <=n; i++)
+    {
+        p=p*i;
+    }
+    for ( j=1,q=1; j <=(n-r); j++)
+    {
+      q=q*j;
+    }
+    
+    t=p/q;
+    return t;
+}
 
 }
